405c40ec83951f73909ea2590ee0f2e0809ad10f
zyxforever <384498377@163.com>
2021-11-16 08:24:21 +0800

    update
    update

--- demo.m
+++ demo.m
@@ -1,12 +1,11 @@
 load("data/data_MNIST10k.mat");
-X=data;Y=labels;
-
-o=20;
+o=10;
 t=20;
-beta=1;
+beta=0.99;
 lambda=0.1;
 kk=10;
 m=90;
+X=data;Y=labels;
 [n,~]=size(X);
 k=length(unique(Y));
 p=min(10*k,n);
